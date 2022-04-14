f77bd0a03b1b9d92ce0eec2c0b03f7d031e5a315
Jookie262 <barrientos.134038@iloilo.sti.edu.ph>
2022-04-14 21:19:46 +0800

    Link Assets in the Project

--- application/modules/dtrscan/views/DTR_View.php
+++ application/modules/dtrscan/views/DTR_View.php
@@ -1,1 +1,27 @@
-<h1>DTR Scanner</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Css -->
+	<link href="<?= base_url('assets/scanner/bootstrap4/css/bootstrap.min.css')?>" rel="stylesheet" type="text/css" />
+	<link href="<?= base_url('assets/scanner/css/animate.min.css')?>" rel="stylesheet" type="text/css" />
+	<script src="<?=base_url('assets/scanner/scanner_design.css')?>" type="text/javascript"></script>
+
+	<!-- Title -->
+	<title> DTR Module </title>
+
+</head>
+<body>
+
+
+	
+	<!-- Javascript -->
+	<script src="<?=base_url('assets/scanner/js/jquery.min.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('assets/scanner/bootstrap4/js/bootstrap.min.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('assets/scanner/js/jsQR.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('assets/scanner/js/lottie-player.js')?>" type="text/javascript"></script>
+    <script src="<?=base_url('assets/scanner/scanner_script.js')?>" type="text/javascript"></script>
+</body>
+</html>
